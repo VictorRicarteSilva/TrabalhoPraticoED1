c085d54b0204150df07b70c02464997a1421454f
Izabela Pereira Maestri <43146605+IzabelaMaestri@users.noreply.github.com>
2023-01-28 15:31:08 -0300

    Create pilha.h

---
+++ pilha.h
@@ -0,0 +1,20 @@
+#ifndef pilha_h // Verifica se a pilha_h existe no escopo
+#define pilha_h // Caso não exista no escopo defini pilha_h
+
+typedef struct node Node;
+typedef struct pilha pilha;
+
+struct pilha {
+  int tam;
+  Node *no;
+};
+
+pilha *criaPilha_Chocolate(); // aloca a Pilha na mémoria
+int empilha_Chocolate(pilha *P, char *chocolate); // insere chocolate na pilha
+int retiraPilha_Chocolate(pilha *P);              // retira da pilha
+void imprimeTopo(pilha *P);                       // imprime o topo da pilha
+int pilhaVazia(pilha *P);    // verifica se a pilha esta vazia
+int pilhaCheia(pilha *P);    // verifica se a pilha esta cheia
+void destroiPilha(pilha *P); // libera a pilha da mémoria
+
+#endif
