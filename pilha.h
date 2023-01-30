338937d4dd79b131219de3c2c44a4ebbc710ca28
Izabela Pereira Maestri <43146605+IzabelaMaestri@users.noreply.github.com>
2023-01-29 21:50:30 -0300

    Update pilha.h

--- pilha.h
+++ pilha.h
@@ -4,17 +4,17 @@
 typedef struct node Node;
 typedef struct pilha pilha;
 
-struct pilha {
+struct pilha { 
   int tam;
   Node *no;
 };
 
 pilha *criaPilha_Chocolate(); // aloca a Pilha na mémoria
 int empilha_Chocolate(pilha *P, char *chocolate); // insere chocolate na pilha
-int retiraPilha_Chocolate(pilha *P);              // retira da pilha
-void imprimeTopo(pilha *P);                       // imprime o topo da pilha
-int pilhaVazia(pilha *P);    // verifica se a pilha esta vazia
-int pilhaCheia(pilha *P);    // verifica se a pilha esta cheia
+int retiraPilha_Chocolate(pilha *P); // retira da pilha
+void imprimeTopo(pilha *P); // imprime o topo da pilha
+int pilhaVazia(pilha *P); // verifica se a pilha esta vazia
+int pilhaCheia(pilha *P); // verifica se a pilha esta cheia
 void destroiPilha(pilha *P); // libera a pilha da mémoria
 
 #endif
