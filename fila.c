0ee7333211ad683868df24d4671653ca878de60c
Izabela Pereira Maestri <43146605+IzabelaMaestri@users.noreply.github.com>
2023-01-29 21:47:43 -0300

    Update fila.c

--- fila.c
+++ fila.c
@@ -2,31 +2,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node { // definição do tipo do no, contendo um cliente e um ponteiro pro
-              // proximo no
+struct node { //definição do tipo do no, contendo um cliente e um ponteiro pro proximo no
   Pessoa cliente;
   struct node *next;
 };
 
-Fila *criaFila() {
+Fila *criaFila() { //cria uma fila 
   Fila *f;
   f = malloc(sizeof(Fila));
   return f;
 }
 
-int filaCheia(Fila *F) {
-  return 1; // para este codigo assumimos que nossa fila nunca estará cheia
+int filaCheia(Fila *F) { //para este codigo assumimos que nossa fila nunca estará cheia
+  return 1; 
 }
 
-int filaVazia(Fila *F) {
-  if (F->inicio ==
-      NULL) { // verifica se o incio da fila e nulo (nenhum elemento na fila)
-    return 1; // fila vazia
+int filaVazia(Fila *F) { //verifica se fila esta vazia 
+  if (F->inicio == NULL) { 
+    return 1; 
   }
-  return 0; // há ao menos um elemento na lista
+  return 0; 
 }
 
-int insereNaFila(Fila *F, Pessoa p) {
+int insereNaFila(Fila *F, Pessoa p) { //insere na fila usando a regra FIFO
   Node *n = malloc(sizeof(Node));
   if (n == NULL)
     return 0;
@@ -39,21 +37,22 @@ int insereNaFila(Fila *F, Pessoa p) {
   F->fim = n;
   return 1;
 }
-int retiraDaFila(Fila *F) {
+
+int retiraDaFila(Fila *F) { //retira da fila pela regra FIFO
   Node *aux = F->inicio;
   F->inicio = F->inicio->next;
   free(aux);
   return 1;
 }
 
-int PrimeiroDaFila(Fila *F, Pessoa *p) {
+int PrimeiroDaFila(Fila *F, Pessoa *p) { //Mostra o primeiro da fila 
   if (F == NULL)
     return 0;
   *p = F->inicio->cliente;
   return 1;
 }
 
-int deletaFila(Fila *F) {
+int deletaFila(Fila *F) { //Deleta a fila e libra o espaço dela da memoria 
   if(F==NULL)return 0;
   while(!filaVazia(F)){
     retiraDaFila(F);
