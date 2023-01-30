67e30a968cfc9da8b1f0111e4efbadb0b6506f0a
Izabela Pereira Maestri <43146605+IzabelaMaestri@users.noreply.github.com>
2023-01-29 21:48:15 -0300

    Update fila.h

--- fila.h
+++ fila.h
@@ -5,23 +5,23 @@
 
 typedef struct node Node;
 
-typedef struct pessoa { // define o tipo de dado Pessoa
+typedef struct pessoa { //define o tipo de dado Pessoa
   char nome[50];
   lista *comanda;
 } Pessoa;
 
-typedef struct fila { // define o tipo de dado fila
+typedef struct fila { //define o tipo de dado fila
   Node *inicio;
   Node *fim;
 } Fila;
 
 
-Fila *criaFila();                     // aloca uma Fila no mémoria
-int filaCheia(Fila *F);               // verifica se a fila esta cheia
-int filaVazia(Fila *F);               // verifica se a fila esta vazia
-int insereNaFila(Fila *F, Pessoa p);  // insere uma nova Pessoa na fila
+Fila *criaFila(); // aloca uma Fila no mémoria
+int filaCheia(Fila *F); // verifica se a fila esta cheia
+int filaVazia(Fila *F); // verifica se a fila esta vazia
+int insereNaFila(Fila *F, Pessoa p); // insere uma nova Pessoa na fila
 int retiraDaFila(Fila *F); // retira uma pessoa da fila
 int PrimeiroDaFila(Fila *F, Pessoa *p); // mostra o primeiro cliente na fila do caixa
-int deletaFila(Fila *F);       // libera uma fila da mémoria
+int deletaFila(Fila *F); // libera uma fila da mémoria
 
 #endif
